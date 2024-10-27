0547b87c3e727fa0f850a8e90c5928571a1f2080
LULUKit <123129846+SAMISsw@users.noreply.github.com>
2024-10-27 09:49:31 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -35,7 +35,7 @@ let package = Package(
         // Targets can depend on other targets in this package,
         // and on products in packages which this package depends on.
         .target(
-            name: "Pixel",
+            name: "PixelKit",
             dependencies: [
             ],
             path: "Sources/MyLibraryName/",
@@ -47,7 +47,7 @@ let package = Package(
             ]
         ),
         .testTarget(
-            name: "Pixel",
+            name: "PixelKit",
             dependencies: [
                 "Pixel",
             ],
