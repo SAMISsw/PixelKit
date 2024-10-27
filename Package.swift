6d1514f4a183f5e2eb0c90ea8451fe1c291b470d
LULUKit <123129846+SAMISsw@users.noreply.github.com>
2024-10-27 09:47:01 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyPackageName",
+    name: "Pixel",
     defaultLocalization: "en",
     platforms: [
         .iOS(.v15),
@@ -15,9 +15,9 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MyLibraryName",
+            name: "Pixel",
             targets: [
-                "MyLibraryName",
+                "Pixel",
             ]
         ),
     ],
@@ -35,7 +35,7 @@ let package = Package(
         // Targets can depend on other targets in this package,
         // and on products in packages which this package depends on.
         .target(
-            name: "MyLibraryName",
+            name: "Pixel",
             dependencies: [
             ],
             path: "Sources/MyLibraryName/",
@@ -47,9 +47,9 @@ let package = Package(
             ]
         ),
         .testTarget(
-            name: "MyLibraryNameTests",
+            name: "Pixel",
             dependencies: [
-                "MyLibraryName",
+                "Pixel",
             ],
             path: "Tests/MyLibraryName/",
             exclude: [
